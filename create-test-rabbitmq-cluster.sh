e48c7ea807989d454db30e58a4cf69fc7ad102f4
daniele985@gmail.com <dpalaia@dpalaia-a02.vmware.com>
2022-05-24 16:59:18 +0200

    commit

---
+++ create-test-rabbitmq-cluster.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+NAMESPACE=${1:-"rabbitmq-system"}
+
+printf "%bCreating RabbitmqCluster...%b\n" "$GREEN" "$NO_COLOR"
+cat << EOF | kapp deploy -a my-tanzu-rabbit -f- -y
+---
+apiVersion: rabbitmq.com/v1beta1
+kind: RabbitmqCluster
+metadata:
+  name: my-tanzu-rabbit
+  namespace: ${NAMESPACE}
+spec:
+  replicas: 1
+  imagePullSecrets:
+  - name: tanzu-rabbitmq-registry-creds
+  securityContext: {}
+EOF
+
+printf "%bWaiting for RabbitmqCluster to report AllReplicasReady...%b\n" "$GREEN" "$NO_COLOR"
+kubectl -n "$NAMESPACE" wait --for=condition=AllReplicasReady rabbitmqcluster/my-tanzu-rabbit --timeout=20m
