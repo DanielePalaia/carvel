b5995365c9df6568f90c303a7117ab8569d45eca
daniele985@gmail.com <dpalaia@dpalaia-a02.vmware.com>
2022-05-25 14:07:51 +0200

    commit

--- create-test-rabbitmq-cluster.sh
+++ create-test-rabbitmq-cluster.sh
@@ -8,6 +8,7 @@ RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
 NAMESPACE=${1:-"rabbitmq-system"}
+ENVIRONMENT=${1:-"tanzu"}
 
 printf "%bCreating RabbitmqCluster...%b\n" "$GREEN" "$NO_COLOR"
 if [[ $ENVIRONMENT != "openshift" ]]; then
