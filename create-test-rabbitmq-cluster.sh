4e2b0e788d5ac3f3a3ad0b173c335de9524b1ee0
daniele985@gmail.com <dpalaia@dpalaia-a02.vmware.com>
2022-05-25 14:05:46 +0200

    commit

--- create-test-rabbitmq-cluster.sh
+++ create-test-rabbitmq-cluster.sh
@@ -10,6 +10,20 @@ NO_COLOR='\033[0m'
 NAMESPACE=${1:-"rabbitmq-system"}
 
 printf "%bCreating RabbitmqCluster...%b\n" "$GREEN" "$NO_COLOR"
+if [[ $ENVIRONMENT != "openshift" ]]; then
+cat << EOF | kapp deploy -a my-tanzu-rabbit -f- -y
+---
+apiVersion: rabbitmq.com/v1beta1
+kind: RabbitmqCluster
+metadata:
+  name: my-tanzu-rabbit
+  namespace: ${NAMESPACE}
+spec:
+  replicas: 1
+  imagePullSecrets:
+  - name: tanzu-rabbitmq-registry-creds
+EOF
+else
 cat << EOF | kapp deploy -a my-tanzu-rabbit -f- -y
 ---
 apiVersion: rabbitmq.com/v1beta1
@@ -29,6 +43,7 @@ spec:
             containers: []
             securityContext: {}
 EOF
+fi
 
 printf "%bWaiting for RabbitmqCluster to report AllReplicasReady...%b\n" "$GREEN" "$NO_COLOR"
 kubectl -n "$NAMESPACE" wait --for=condition=AllReplicasReady rabbitmqcluster/my-tanzu-rabbit --timeout=20m
--- undeploy-package-repo.sh
+++
@@ -1,3 +0,0 @@
-yes | kapp delete -a kc 
-yes | kapp delete -a sg 
-kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
