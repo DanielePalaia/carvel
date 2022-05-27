b7b01f18573ca288b6ecf3bf79758694bba7ac9d
daniele985@gmail.com <dpalaia@dpalaia-a02.vmware.com>
2022-05-27 14:29:37 +0200

    commit

--- deploy-package-repo.sh
+++ deploy-package-repo.sh
@@ -55,7 +55,7 @@ if [[ "$ENVIRONMENT" != "openshift" ]]; then
     popd
 
     else
-      overlay_path=$(dirname -- "$0";)/openshift-overlay
+      export overlay_path=$(dirname -- "$0";)/openshift-overlay
       ytt -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml -f $overlay_path/kapp-overlay.yml > $overlay_path/kapp.yml
       ytt -f https://github.com/vmware-tanzu/carvel-secretgen-controller/releases/latest/download/release.yml -f $overlay_path/secretgen-overlay.yml > $overlay_path/overlay/secretgen.yml
       kapp deploy -y -a kc -f $overlay_path/kapp.yml
